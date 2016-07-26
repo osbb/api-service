a1bb7776ee491f1eed84e67de03a8af25b6ad33a
Dmytro Les <dmitriy.s.les@gmail.com>
2016-07-26 19:18:59 +0300

    linting

--- module.js
+++ module.js
@@ -1,51 +1,56 @@
-'use strict';
-
 const Hapi = require('hapi');
 const Promise = require('bluebird');
 const seneca = require('seneca');
+const winston = require('winston');
 
 const server = new Hapi.Server();
-server.connection({ port: 3000 });
+
+server.connection({
+  port: 3000,
+  routes: {
+    cors: true,
+  },
+});
 
 const senecaClient = seneca()
-    .client({
-        host: process.env.USERS_PORT_10101_TCP_ADDR,
-        port: process.env.USERS_PORT_10101_TCP_PORT,
-    })
-    .client({
-        host: process.env.COOPERATIVES_PORT_10101_TCP_ADDR,
-        port: process.env.COOPERATIVES_PORT_10101_TCP_PORT,
-    });
+  .client({
+    host: process.env.USERS_PORT_10101_TCP_ADDR,
+    port: process.env.USERS_PORT_10101_TCP_PORT,
+  })
+  .client({
+    host: process.env.COOPERATIVES_PORT_10101_TCP_ADDR,
+    port: process.env.COOPERATIVES_PORT_10101_TCP_PORT,
+  });
 
 const act = Promise.promisify(senecaClient.act, { context: senecaClient });
 
 server.route({
-    method: 'GET',
-    path: '/users',
-    handler: (req, done) => {
-        act({ role: 'users', cmd: 'find' })
-            .then(res => done(res));
-    }
+  method: 'GET',
+  path: '/users',
+  handler: (req, done) => {
+    act({ role: 'users', cmd: 'find' })
+      .then(res => done(res));
+  },
 });
 
 server.route({
-    method: 'POST',
-    path: '/cooperatives',
-    config: {
-        payload: {
-            output: 'data',
-            parse: true,
-        },
+  method: 'POST',
+  path: '/cooperatives',
+  config: {
+    payload: {
+      output: 'data',
+      parse: true,
     },
-    handler: (req, done) => {
-        act({ role: 'cooperative', cmd: 'create', cooperative: req.payload })
-            .then(res => done(res));
-    }
+  },
+  handler: (req, done) => {
+    act({ role: 'cooperative', cmd: 'create', cooperative: req.payload })
+      .then(res => done(res));
+  },
 });
 
 server.start((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('Server running at:', server.info.uri);
+  if (err) {
+    throw err;
+  }
+  winston.info(`Server running at: ${server.info.uri}`);
 });
